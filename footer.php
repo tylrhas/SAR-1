28e9f42b9abff4d47819079b59ab7fb8c9fb842b
Tyler Hasenoehrl <tylrhas@gmail.com>
2020-12-01 21:00:22 -0800

    remove mailchimp

--- footer.php
+++ footer.php
@@ -7,12 +7,12 @@
             <p><strong>telephone:</strong>(519) 501-2037</p>
             <p><strong>email:</strong> info@sar-1.ca</p>
         </div>
-        <div class="col-md-6 padding-norm">
+        <!-- <div class="col-md-6 padding-norm">
         	<h2 class="footer_head">subscribe to<br/><span>our newletter</span></h2>
 			<?php 
 			// mailchimpSF_signup_form(); 
 			?>
-        </div>
+        </div> -->
     </div>
 </div>
 <div class="row dark_gray">
